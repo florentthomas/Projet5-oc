8162ce3c23a99321051023736efbd1f61bee1c4f
Florent THOMAS <florentthomas78@gmail.com>
2021-05-20 21:51:01 +0200

    rectification erreur syntaxe

--- App/Models/Manager.php
+++ App/Models/Manager.php
@@ -7,7 +7,7 @@ abstract class Manager{
 
     private function connect_bdd(){
 
-        $bdd=new \PDO("mysql:host=localhost;dbname:projet_5_oc;charset=utf8","root";"");
+        $bdd=new \PDO("mysql:host=localhost;dbname:projet_5_oc;charset=utf8","root","");
         array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         return $bdd;
     }
--- index.php
+++ index.php
@@ -12,11 +12,9 @@ $router=new Router($_GET['url']);
 
 
 $router->get("/","BlogController@index");
-<<<<<<< HEAD
-$router->get("/blog/:slug-:id","ArticleController@show");
-=======
+
 $router->get("/blog/:slug-:id","ArticleController@index");
->>>>>>> page_article
+
 
 $router->run();
 
