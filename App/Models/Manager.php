87468549818a267b313b93927bf456eeb2f0d70a
Florent THOMAS <florentthomas78@gmail.com>
2021-05-25 00:41:35 +0200

    creation globals config pour la connection bdd

--- App/Models/Manager.php
+++ App/Models/Manager.php
@@ -4,11 +4,23 @@ namespace App\Models;
 
 abstract class Manager{
 
+    private $_user;
+    private $_host;
+    private $_password;
+    private $_db_name;
+
+    public function __construct(){
+        $this->_user=USER;
+        $this->_host=HOST;
+        $this->_password=PASSWORD;
+        $this->_db_name=DB_NAME;
+    }
+
 
     protected function connect_bdd(){
 
         try{
-            $bdd=new \PDO("mysql:host=localhost;dbname=projet_5_oc;charset=utf8","root","",
+            $bdd=new \PDO("mysql:host=".$this->_host.";dbname=".$this->_db_name.";charset=utf8",$this->_user,$this->_password,
             array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ));
             
--- App/config/config.php
+++ App/config/config.php
@@ -4,3 +4,9 @@
 
 define('PATH_VIEWS_FOLDER',str_replace('index.php','App/Views',$_SERVER['SCRIPT_FILENAME']));
 
+//Config_db
+
+define('HOST','localhost');
+define('USER','root');
+define('PASSWORD','');
+define('DB_NAME','projet_5_oc');
