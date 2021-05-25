6078fde3bb6401a278beb0778c361f5f02b770cb
Florent THOMAS <florentthomas78@gmail.com>
2021-05-25 03:16:59 +0200

    execution des methodes de la classe Manager

--- App/Models/ArticleManager.php
+++ App/Models/ArticleManager.php
@@ -7,28 +7,10 @@ class ArticleManager extends Manager{
 
     public function getArticle($id){
 
-        $sql="SELECT * FROM articles WHERE id = :id";
-
-        // $query=$this->connect_bdd()->prepare($sql);
-        // $query->execute(Array("id" => $id));
-
         $this->prepare("SELECT * FROM articles WHERE id =:id");
         $this->execute_query_prepared(["id" => $id]);
+        $result=$this->get_one();
 
-        $test=$this->get_one();
-
-        var_dump($test);
-
-        $data=$query->fetch();
-
-        if($data){
-
-            $article=new ArticleModel($data);
-            return $data;
-        }
-    
-        return false;
-
-        
+        return $result;
     }
 }
