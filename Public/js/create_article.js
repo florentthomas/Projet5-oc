5a6b195146b2b931f1844e5a956fdeab85c8ab4b
Florent THOMAS <florentthomas78@gmail.com>
2022-10-11 00:14:03 +0200

    ajout controle de la taille et extension de l'upload d'une image en js

--- Public/js/create_article.js
+++ Public/js/create_article.js
@@ -1,14 +1,57 @@
 jQuery(document).ready(function($){
 
+    
+    let image_accepted=false;
+
+
+    $("#image_article").change(function(){
+
+        const messageElt=$("#img_check");
+
+        const image= this.files[0];
+
+        const image_size= image.size / 1024;
+
+        if(image_size < 2048){
+
+            const allowedExtension = ['jpeg', 'jpg', 'png', 'gif'];
+            
+            const image_extension= image.name.split(".").pop().toLowerCase();
+
+
+            for(let i = 0; i < allowedExtension.length; i++){
+
+                if(image_extension === allowedExtension[i]){
+                    image_accepted= true;
+                    messageElt.html("image acceptée");
+                    break;
+                }
+                else{
+                    image_accepted= false;
+                    messageElt.html("Format non conforme (jpg, png, gif)");
+                }
+
+            }
+        }
+
+        else{
+            messageElt.html("L'image est trop grande (2Mo max)");
+            image_accepted=false;
+        }
+
+    })
+
+
+
 
     $("#create_article").submit(function(e){
 
+    
         const content=CKEDITOR.instances['article'].getData();
-        const image_article=$("#image_article");
         const result_elt=$("#result");
         
-
-        if($("#title_article").val().length === 0 || $("#slug_article").val().length === 0 || $("#description").val().length === 0 ){
+        
+        if($("#title_article").val().length === 0 || $("#slug_article").val().length === 0 || $("#description").val().length === 0 || content.length === 0){
             e.preventDefault();
             result_elt.addClass("error");
             result_elt.text("Les champs ne sont pas tous remplis");
@@ -16,22 +59,8 @@ jQuery(document).ready(function($){
            
         }else{
 
-            const allowedExtension = ['jpeg', 'jpg', 'png', 'gif'];
-            
-            const image_extension= image_article.val().split(".").pop().toLowerCase();
 
-            let extension_valide=false;
-
-            for(let i = 0; i < allowedExtension.length; i++){
-
-                if(image_extension === allowedExtension[i]){
-                    extension_valide= true;
-                    break;
-                }
-
-            }
-
-            if(extension_valide === false){
+            if(image_accepted === false){
                 e.preventDefault();
                 result_elt.addClass("error");
                 result_elt.text("Aucune image sélectionnée ou le format n'est pas correct");
