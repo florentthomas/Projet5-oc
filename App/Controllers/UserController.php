9eeb96e527bc6d46b9144f9c3d108631f854a6df
Florent THOMAS <florentthomas78@gmail.com>
2022-10-11 02:07:22 +0200

    correction du nom de la photo profil par defaut

--- App/Controllers/UserController.php
+++ App/Controllers/UserController.php
@@ -299,7 +299,7 @@ class UserController extends Controller{
 
                 $this->userManager->update_user("photo",$name_photo,$current_user->id);
 
-                if($_SESSION["user"]->photo !== "default.svg"){
+                if($_SESSION["user"]->photo !== "Default.svg"){
                     unlink(PATH_IMG_AVATARS."/".$_SESSION["user"]->photo);
                 }
                 
