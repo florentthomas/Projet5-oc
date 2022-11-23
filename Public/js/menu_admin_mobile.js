40989ce73875cf2bd17f073a37eb430fa8d5f4f6
Florent THOMAS <florentthomas78@gmail.com>
2022-11-23 02:08:29 +0100

    mis en place du menu de la partie admin en responsive design

---
+++ Public/js/menu_admin_mobile.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){
+
+
+
+  
+
+    $("#menu_admin_mobile").click(()=>{
+
+        
+        $("#list_menu_admin").toggleClass("menu_admin_mobile")
+    
+        
+        
+    })
+
+    
+})
